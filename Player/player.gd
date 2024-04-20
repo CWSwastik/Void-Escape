799f36750bad6565f25bcadbe4ea25f79384c270
TheMaster <69687013+TheMaster@users.noreply.github.com>
2024-04-20 23:08:51 +0530

    Implemented Dash
    TO DO:
    Air dash
    Fix animation (optional?)

--- Player/player.gd
+++ Player/player.gd
@@ -3,9 +3,12 @@ extends CharacterBody2D
 
 var killed = false
 const SPEED = 200.0
+const DASH = 500.0
 const JUMP_VELOCITY = -435.0
 @onready var SPRITE = $AnimatedSprite2D
 var facing_left = false
+var dashing = false
+var dash_cooldown = 10
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
@@ -26,7 +29,7 @@ func _physics_process(delta):
 			SPRITE.animation = "Jump"
 	elif (abs(velocity.x) > 1):
 		SPRITE.animation = "Run"
-	else:
+	elif not dashing:
 		SPRITE.animation = "Idle"
 	
 
@@ -37,18 +40,37 @@ func _physics_process(delta):
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 		
+	if dash_cooldown < 0:
+		dashing = false
 	
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction = Input.get_axis("move_left", "move_right")
+	var sp = SPEED
+	
+	if Input.is_action_just_pressed("dash"):
+		sp = DASH
+		dashing = true
+		dash_cooldown = 10
+		SPRITE.animation = "Dash"
+		
+	if dashing:
+		sp = DASH
+		dash_cooldown -= 1
+		if facing_left:
+			velocity.x = -sp
+		else:
+			velocity.x = sp
+		
 	if direction:
-		velocity.x = direction * SPEED
+		velocity.x = direction * sp
 		if direction < 0:
 			facing_left = true
 		else:
 			facing_left = false
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
+		
 
 
 	move_and_slide()
