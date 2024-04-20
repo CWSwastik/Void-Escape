185d304057ad6d4ae8fa75315b89f859a000db0c
Swas.py <codewithswastik@gmail.com>
2024-04-20 15:47:06 +0530

    Implement player, basic map

---
+++ Player/player.gd
@@ -0,0 +1,45 @@
+extends CharacterBody2D
+
+
+const SPEED = 200.0
+const JUMP_VELOCITY = -435.0
+@onready var SPRITE = $AnimatedSprite2D
+
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
+
+func _physics_process(delta):
+	
+	if not is_on_floor():
+		if (abs(velocity.x) > 500): # TODO: Figure this out
+			SPRITE.animation = "JumpFlip"
+		else:
+			SPRITE.animation = "Jump"
+	elif (abs(velocity.x) > 1):
+		SPRITE.animation = "Run"
+	else:
+		SPRITE.animation = "Idle"
+	
+
+	if not is_on_floor():
+		velocity.y += gravity * delta
+	
+	# Handle jump.
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+		
+	
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var direction = Input.get_axis("move_left", "move_right")
+	if direction:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+
+
+	move_and_slide()
+	
+	
+	SPRITE.flip_h = velocity.x < 0
